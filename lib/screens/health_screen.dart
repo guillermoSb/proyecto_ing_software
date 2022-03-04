7b791e45fab6bf0ab124f2b5e98810a125831df6
= <guillermo.santosba@gmail.com>
2022-03-04 08:54:27 -0600

    Implemented Health page

--- lib/screens/health_screen.dart
+++ lib/screens/health_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:health_app/themes/light_theme.dart';
 import 'package:health_app/widgets/pageTitle_widget.dart';
 
 class HealthScreen extends StatelessWidget {
@@ -11,7 +12,53 @@ class HealthScreen extends StatelessWidget {
         child: Column(
       children: [
         PageTitleWidget('Salud'),
+        Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              ProgressWidget("Ejercicio"),
+              SizedBox(
+                height: 24,
+              ),
+              ProgressWidget("Meditar")
+            ],
+          ),
+        )
       ],
     ));
+  }
+}
+
+class ProgressWidget extends StatelessWidget {
+  final String title;
+  const ProgressWidget(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [Text("$title"), Text("20/5 días")],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            child: const LinearProgressIndicator(
+              value: 0.4,
+              color: AppTheme.primary,
+              backgroundColor: AppTheme.secondary,
+              minHeight: 20,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
